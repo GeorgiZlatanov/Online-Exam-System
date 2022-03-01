8869a5fa9911f37ce0406bc0b96482e9359186a4
GeorgiZlatanov <43550453+GeorgiZlatanov@users.noreply.github.com>
2022-03-01 14:00:06 +0200

    Feature (#3)
    * Creates Models
    
    * Create Models
    
    * Creates ApplicationDbContext and set some validations in the class
    
    * Create and implement Generic Repository

---
+++ OnlineExaminationSystem/OnlineExamination.DataAccess/Repository/GenericRepository.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineExamination.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExamination.DataAccess.Repository
+{
+    public class GenericRepository<T> : IDisposable, IGenericRepository<T> where T : class
+    {
+        internal DbSet<T> dbSet;
+        private readonly ApplicationDbContext _context = null;
+
+        public GenericRepository(DbSet<T> dbSet, ApplicationDbContext context)
+        {
+            this.dbSet = dbSet;
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            dbSet.Add(entity);
+            return entity;
+        }
+
+        public void Delete(T entityToDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync(T entityToDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteByID(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null, 
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, 
+            string includeProperties = "")
+        {
+            IQueryable<T> query = dbSet;
+            if (filter!=null)
+            {
+                query = query.Where(filter);
+            }
+            foreach (var includeProperty in includeProperties.Split(new char[] { ','}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+            if (orderBy!=null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public T GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public async Task<T> GetByIdAsync(object id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public void Update(T entityToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entityToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
