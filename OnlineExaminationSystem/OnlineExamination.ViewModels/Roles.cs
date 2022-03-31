b2b4cd6535270c66b003510920f7201c8dcbb8aa
Kristiqn Petkov <krisio123123@gmail.com>
2022-03-31 21:05:29 +0300

    All viewModel classed added

---
+++ OnlineExaminationSystem/OnlineExamination.ViewModels/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineExamination.ViewModels
+{
+    public enum EnumRoles
+    {
+        //rename Roles to EnumRoles if there is some error
+        Admin=1,
+        Teacher=2,
+        Student=3
+    }
+}
