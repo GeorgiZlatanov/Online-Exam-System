da567aa5139ba5a282e157e3fc3a2aa5487fe657
kripri <62754674+kristiyanpetkovvv@users.noreply.github.com>
2022-03-31 20:35:40 +0300

    new View models (#9)
    Ok!

---
+++ OnlineExaminationSystem/OnlineExamination.ViewModels/GroupViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineExamination.DataAccess;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineExamination.ViewModels
+{
+    public class GroupViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        public int UsersId { get; set; }
+        public List<GroupViewModel> GroupList { get; set; }
+        public int TotalCount { get; set; }
+
+        public GroupViewModel(Groups model)
+        {
+            Id = model.Id;
+            Name = model.Name ?? "";
+            Description = model.Description ?? "";
+            UsersId = model.UsersId;
+        }
+
+        public Groups ConvertViewModel(GroupViewModel vm)
+        {
+            return new Groups
+            {
+                Id = vm.Id,
+                Name = vm.Name ?? "",
+                Description = vm.Description ?? "",
+                UsersId = vm.UsersId
+            };
+        }
+    }
+}
