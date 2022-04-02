2beaaf374219324cdac968575bc82c505bc67f63
kripri <62754674+kristiyanpetkovvv@users.noreply.github.com>
2022-04-02 13:03:40 +0300

    all viewModel classes comleted (#10)
    * new View models
    
    * All viewModel classed added
    
    * video 8 is done

---
+++ OnlineExaminationSystem/OnlineExamination.ViewModels/StudentCheckBoxListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineExamination.DataAccess;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineExamination.ViewModels
+{
+    public class StudentCheckBoxListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Selected { get; set; }
+    }
+}
