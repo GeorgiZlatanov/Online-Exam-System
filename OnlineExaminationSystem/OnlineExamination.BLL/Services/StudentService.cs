ad3758f1e9cd1480196b3ec39bbeeebc87e0e9c5
konstant2003 <56306158+konstant2003@users.noreply.github.com>
2022-04-02 16:15:04 +0300

    Add logic to StudentService class (#11)
    OK!

---
+++ OnlineExaminationSystem/OnlineExamination.BLL/Services/StudentService.cs
@@ -0,0 +1,112 @@
+﻿using OnlineExamination.DataAcces.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineExamination.BLL.Services
+{
+    public class StudentService : IStudentService
+    {
+        IUnitOfWork _unitOfWork;
+        ILogger<StudentService> _iLogger;
+
+        public StudentService(IUnitOfWork unitOfWork, ILogger<StudentService> iLogger)
+        {
+            _unitOfWork = unitOfWork;
+            _iLogger = iLogger;
+        }
+
+        public Task<StudentViewModel> AddAsync(StudentViewModel vm)
+        {
+            try
+            {
+                Students ogj = vm.ConvertViewModel(vm);
+                await _unitOfWork.GenericRepository<Students>().AddAsync(obj);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return vm;
+        }
+
+        public PagedResult<StudentViewModel> GetAll(int pageNumber, int pageSize)
+        {
+            var model = new StudentViewModel();
+            try
+            {
+                int ExcludeRecords = (pageSize * pageNumber) - pageSize;
+                List<StudentViewModel> detailList = new List<StudentViewModel>();
+                var modelList = _unitOfWork.GenericRepository<Students>().GetAll().Skip(ExcludeRecords).Take(pageSize).ToList;
+                var totalCount = _unitOfWork.GenericRepository<Students>().GetAll().ToList();
+                detailList = GroupListInfo(modelList);
+                if (detailList != null)
+                {
+                    model.StudentList = detailList;
+                    model.TotalCount = totalCount.Count();
+                }
+            }
+            catch (Exception ex)
+            {
+                _iLogger.LogError(ex.Message);
+            }
+            var result = new PagedResult<StudentViewModel>
+            {
+                Data = model.StudentList,
+                TotalItems = model.TotalCount,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+            return result;
+        }
+
+        private List<StudentViewModel> GroupListInfo(List<Students> modelList)
+        {
+            return modelList.Select(o => new StudentViewModel(o)).ToList();
+        }
+
+        public IEnumerable<Student> GetAllstudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ResultViewModel> GetExamResults(int studentId)
+        {
+            try
+            {
+                var examResults = _unitOfWork.GenericRepository<ExamResults>().GetAll().Where(a => a.StudentsId == studentId);
+                var students = _unitOfWork.GenericRepository<Students>().GetAll();
+                var exams = _unitOfWork.GenericRepository<ExamResults>.GetAll();
+                var qnas = _unitOfWork.GenericRepository<QnAs>().GetAll();
+
+                var requiredData = examResults.Join(students, er => er.StudentsId, s => s.Id,
+                    (er, st) => new { er, st }).Join(exams, erj => erj.er.ExamsId, ex => ex.Id,
+                    (erj, ex) => new { erj, ex }).Join(qnas, exj => exj.erj.er.QnAsId, q => q.Id,
+                    (exj, q) => new ResultViewModel()
+                    { });
+                return requiredData;
+            }
+            catch (Exception ex)
+            {
+                _iLogger.LogError(ex.Message);
+                
+            }
+            return Enumerable.Empty<ResultViewModel>();
+        }
+
+        public StudentViewModel GetStudentDetails(int studentId)
+        {
+            try
+            {
+                var student = _unitOfWork.GenericRepository<Students>().GetByID(studentId);
+                return student != null ? new StudentViewModel(student) : null;
+            }
+            catch (Exception ex)
+            {
+                _iLogger.LogError(ex.Message);
+            }
+            return null;
+
+        }
+    }
+}
